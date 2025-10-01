ced1755d40015dedf7de2436c7d7664a6dc1503b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-01 21:35:11 +0500

    update

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs,not browser
+console.log(3+3);
+console.log("haris");
+
+let name = "haris"
+let age = 29
+let IsLoggedIn = true
+
+// number => 2 to power of 53
+//bigint
+//string => ""
+//bolean => true,false
+//null => standalone value
+// undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);
--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,17 +2,15 @@ const accountId = 144533
 let accountEmail = " harisshahnawaz97@gmail.com"
 var accountPassword = "4567"
 accountCity = "lahore"
+let accountState;
 
+// accountId = 2
 
-accountId = 2
-
-// console.log(accountId);//not allowed
+ console.log(accountId);
 accountEmail = "harisshahnawaz57@gmail.com"
 
 accountPassword = "7777"
 accountCity = "karachi"
 
-console.log(accountId);
-
-console.table([accountId,accountEmail,accountPassword,accountCity])
-
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/* prefer not to use  var because of issue in block scope and function scope  */
