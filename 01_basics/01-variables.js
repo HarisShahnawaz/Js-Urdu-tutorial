3cc433365ffe0b99768e2dd5d5029f124488d2a6
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-01 20:29:35 +0500

    update

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 144533
+let accountEmail = " harisshahnawaz97@gmail.com"
+var accountPassword = "4567"
+accountCity = "lahore"
+
+
+accountId = 2
+
+// console.log(accountId);//not allowed
+accountEmail = "harisshahnawaz57@gmail.com"
+
+accountPassword = "7777"
+accountCity = "karachi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
