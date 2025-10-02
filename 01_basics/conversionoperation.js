e38c258a9f9a673a37cb4a36991229dd8e0d58da
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-02 19:20:11 +0500

    update

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,34 @@
+//let score = "33abc" 33abc = NaN
+//let score = null     NULL=0
+//let score = undefined  UNDEFINED=NaN
+//let score = "33"  33=33
+//let score = true      TRUE=1
+//let score = false  false=1
+//let score = "haris" Nan
+//let score = "haris" 
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+//let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+//let IsLoggedIn = 1      1=true and 0= false
+//let IsLoggedIn = ""       false 
+//let IsLoggedIn = "haris"  true
+let IsLoggedIn = 0
+
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+//console.log(typeof booleanIsLoggedIn); 
+//console.log(booleanIsLoggedIn);
+
+let SomeNumber = 33
+let StringNumber = String(SomeNumber)
+
+console.log(StringNumber )
+console.log(typeof StringNumber )
