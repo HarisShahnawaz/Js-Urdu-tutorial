02d893a1aa5e12067c1e67fbf55eed56729c59a4
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-05 19:00:30 +0500

    update

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,6 +1,26 @@
-const name = "haris"
-const RepoCount = 50
+const name = "haris";
+const RepoCount = 50;
 
-console.log(name + RepoCount + " value");
+//console.log(name + RepoCount + " value");
+
+console.log(`hello my name is ${name} and my repocount is ${RepoCount}`);
+
+ const GameName = new String('haris-chh');
+
+ console.log(GameName[0]);
+ console.log(GameName.__proto__);
+
+
+ console.log(GameName.length);
+
+  console.log(GameName.toUpperCase());
+
+  console.log(GameName.charAt(4));
+
+  console.log(GameName.indexOf('r'));
+
+  const newString = GameName.substring(0,4);
+
+  console.log(newString);
 
  
