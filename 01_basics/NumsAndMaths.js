88abe9f5f1e2d917bf61250bde39791f8a5f7b53
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-05 21:07:09 +0500

    update

---
+++ 01_basics/NumsAndMaths.js
@@ -0,0 +1,22 @@
+const score = 400
+
+console.log(score);
+
+
+const balance = new Number('100')
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed('2'));
+
+const otherNumber = 123.8888
+
+console.log( otherNumber.toPrecision(4));
+
+const numbers= 1000000000
+
+console.log(numbers.toLocaleString());
+console.log(numbers.toExponential());
+
+//+++++++++++++++  maths  +++++++++++++++++++++++++++++++++++++++
+
