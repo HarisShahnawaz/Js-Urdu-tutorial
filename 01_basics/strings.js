7e007bb007824c654e941a6513f0c361927ecaca
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-05 20:23:30 +0500

    update

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -3,24 +3,41 @@ const RepoCount = 50;
 
 //console.log(name + RepoCount + " value");
 
-console.log(`hello my name is ${name} and my repocount is ${RepoCount}`);
+// console.log(`hello my name is ${name} and my repocount is ${RepoCount}`);
 
- const GameName = new String('haris-chh');
+  const GameName = new String('haris-chh-aris');
 
- console.log(GameName[0]);
- console.log(GameName.__proto__);
+//  console.log(GameName[0]);
+//  console.log(GameName.__proto__);
 
 
- console.log(GameName.length);
+//  console.log(GameName.length);
 
-  console.log(GameName.toUpperCase());
+//   console.log(GameName.toUpperCase());
 
-  console.log(GameName.charAt(4));
+//   console.log(GameName.charAt(4));
 
-  console.log(GameName.indexOf('r'));
+//   console.log(GameName.indexOf('r'));
 
   const newString = GameName.substring(0,4);
 
-  console.log(newString);
+  //console.log(newString);
 
- 
+  const anotherString = GameName.slice(-6,4);
+
+  //console.log(anotherString);
+
+  const anotherStringone = "      haris         "
+
+  console.log(anotherStringone);
+  console.log(anotherStringone.trim());
+
+
+  const url = "https://haris.com/coding%20shahnawaz"
+
+  console.log(url.replace("%20","-"));
+  
+  console.log(url.includes("code"));
+  
+  console.log(GameName.split('-'));
+  
