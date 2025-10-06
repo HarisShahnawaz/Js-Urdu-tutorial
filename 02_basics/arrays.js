316793b4b0edd4e9fe32517a7aecd8dcaa90e5e0
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-06 20:08:17 +0500

    update

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -68,7 +68,16 @@ const anotherarray = [1,2,3 ,[4,5,6,] ,7,[6, 7, [4,5]]]
 
 const real_anotherarray = anotherarray.flat(3)
 
-console.log(anotherarray );
-console.log(real_anotherarray );
+// console.log(anotherarray );
+// console.log(real_anotherarray );
 
 
+// console.log(Array.isArray("haris") );
+// console.log(Array.from("haris") );
+// // console.log(Array.from({name : "haris"}) ); //////interesting
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
