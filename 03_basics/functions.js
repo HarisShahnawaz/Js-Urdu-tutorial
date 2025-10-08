f8c3e817f6e1991d8e716ba2f8348a6748e8ab55
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-08 19:49:06 +0500

    update

---
+++ 03_basics/functions.js
@@ -0,0 +1,18 @@
+
+function SayMyName(){
+console.log("H");
+console.log("A");
+console.log("R");
+console.log("I");
+console.log("S");
+}
+
+//SayMyName()
+   //SayMyName
+
+   function addtwonum(num1,num2){
+    console.log(num1+num2);
+    
+   }
+        addtwonum(5,8)
+   
