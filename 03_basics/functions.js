e431f96d0a808abf18df3bc53e5f1399dc57c676
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-08 20:18:57 +0500

    update

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -10,9 +10,50 @@ console.log("S");
 //SayMyName()
    //SayMyName
 
-   function addtwonum(num1,num2){
-    console.log(num1+num2);
-    
-   }
-        addtwonum(5,8)
    
+   
+//    function addtwonum(num1,num2){
+    
+//     console.log(num1+num2);
+//    }
+   
+   function addtwonum(num1,num2){
+    
+    // let result = num1+num2
+    // console.log("student");
+
+    return num1 + num2
+    
+    // return result
+   }
+       // addtwonum(5, "6")
+        //addtwonum(5, "a")
+
+        const result = addtwonum(5, 5)
+   
+//    console.log("Result :", result);
+   
+
+function loginusermessage(username)
+{
+    // if(username === undefined){
+    //    console.log("plz enter username");
+    //    return
+    // }
+    if(!username){
+       console.log("plz enter username");
+       return
+    }
+  return `${username} just logged in`
+}
+
+// console.log((loginusermessage("haris")))
+// console.log(loginusermessage(""))
+// console.log(loginusermessage())
+
+function calculateCartprice(num1){
+    return num1
+}
+
+
+
