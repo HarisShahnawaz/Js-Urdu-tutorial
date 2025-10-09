8f5d3b6f17955bbf1634ff8e20bae356887fb98b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-09 20:22:03 +0500

    update

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -7,7 +7,7 @@ let a = 300
 if(true){
 let a = 10
 const b = 20
-console.log("inner :", a) ;
+// console.log("inner :", a) ;
 
 
 }
@@ -20,3 +20,47 @@ console.log("inner :", a) ;
 // nested scope
 
 
+function one(){
+ const username = "haris"
+
+ function two(){
+    const website = "youtube"
+    console.log(username);
+    
+ }
+//   console.log(website);
+  
+  two()
+}
+// one()
+
+if(true){
+    const username ="haris"
+    if(username === "haris"){
+     const website = "  youtube"
+    //  console.log(username + website);
+     
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+//  ***********interesting ********************
+console.log(add1(5));
+
+function add1(num){
+return num + 1
+
+}
+
+
+
+// add2(5)  error Cannot access 'add2' before initialization
+
+const add2 = function(num){
+    return num + 2
+}
+
+console.log(add2(5));
