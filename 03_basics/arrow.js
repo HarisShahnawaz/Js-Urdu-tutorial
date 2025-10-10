1169c464bdc6c648f041c32201a3cb9cee40d467
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-10 22:11:44 +0500

    update

--- 03_basics/arrow.js
+++ 03_basics/arrow.js
@@ -10,9 +10,54 @@ const user = {
 
   }
 }
-user.welcomemessage()
+// user.welcomemessage()
 
-user.username = "sam"
-user.welcomemessage()
+// user.username = "sam"
+// user.welcomemessage()
 
 // console.log(this)
+
+// function chai(){
+//     let username = "haris"
+//      console.log(this.username);
+     
+// }
+//   chai()
+
+
+// const chai = function()
+// {
+//     let username = "haris"
+//     console.log(this.username);
+
+// }
+
+
+ const chai = () =>
+{
+    let username = "haris"
+    console.log(this);
+
+}
+
+
+// chai()
+
+
+// const addtwo = (num1 , num2 ) => {
+   
+// return num1 + num2
+
+// }         method 1
+
+// const addtwo = (num1 , num2 ) => num1 + num2    method 2
+//const addtwo = (num1 , num2 ) => (num1 + num2 )  // method 3
+
+
+const addtwo = (num1 , num2 ) => ({username : "haris"})  
+
+   
+
+
+console.log(addtwo(3,6));
+
