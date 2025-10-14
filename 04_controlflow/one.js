5444c86131ec1b81be35bea037963950b104d326
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-14 21:58:28 +0500

    update

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -11,6 +11,16 @@
 // } // code dont run  bcz condition is false
 
 const isuserloggedin = true
+const temprature = 41
+
+
+if(temprature < 33){
+   console.log("less than 50");
+} 
+else{
+console.log("temprature is higher than 50");
+}
+
 
 if(2=="2"){
    console.log("executed");
@@ -23,5 +33,13 @@ if(2==="2"){
 // 2>=2
 // 3!=2
 
-// <,>,<=,>= ,==,!=,===
+// <,>,<=,>= ,==,!=,===,!==
 
+
+const score = 200
+
+if(score > 100){
+    const power = "fly"
+    console.log(`user power : ${power}`);
+    
+}
