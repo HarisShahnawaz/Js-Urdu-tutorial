e9a8a4a6aa8840e87d10e5221f8d960ba5607e3f
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-14 21:47:06 +0500

    update

---
+++ 04_controlflow/one.js
@@ -0,0 +1,27 @@
+//if
+
+
+// if(true)
+// {
+  
+// }
+// if(false)
+// {
+  
+// } // code dont run  bcz condition is false
+
+const isuserloggedin = true
+
+if(2=="2"){
+   console.log("executed");
+}    // executed bcz of ==
+
+
+if(2==="2"){
+   console.log("executed");
+}
+// 2>=2
+// 3!=2
+
+// <,>,<=,>= ,==,!=,===
+
