f166a59c40fc1b55105cc3aecf861ac22552d69d
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-16 23:15:44 +0500

    update

---
+++ 05_loops/whileloop.js
@@ -0,0 +1,25 @@
+// while
+
+let index = 0
+while (index<= 10) {
+console.log(`value of index is : ${index}`);
+    index = index + 2
+    
+}
+
+let MyArray = ["batman","superman","hulk"]
+   let arr=0
+while (arr < MyArray.length) {
+    console.log(`value is ${MyArray[arr]}`);
+    arr = arr + 1
+}
+
+// do while loop
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score = score+1
+    
+} while (score<=10);
