9c292f531a81d9d5964d416383848e7b45517206
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-17 22:31:05 +0500

    update

--- 05_loops/forOfLoop.js
+++ 05_loops/forOfLoop.js
@@ -3,3 +3,21 @@
 // ["" , "", ""]
 // [{},{},{}]
 
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+const greetings = "hello word"
+
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+    
+}
+
+// maps
+
+const map = new Map()
+map.set('pak',"pakistan")
+map.set('usa',"united states")
