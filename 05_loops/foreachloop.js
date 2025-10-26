d82c6044bba3e60e483bf8983d458184bc18e0ca
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-26 14:47:30 +0500

    update

---
+++ 05_loops/foreachloop.js
@@ -0,0 +1,14 @@
+const coding = ["js","ruby","c++","python","java"]
+
+coding.forEach( function (items) {
+//    console.log(items);
+   
+} )
+    
+
+// coding.forEach( (val) => {console.log(val)});
+function printme(item) {
+    console.log(item);
+}
+
+
