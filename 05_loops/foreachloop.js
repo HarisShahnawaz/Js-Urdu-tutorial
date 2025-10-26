2663bef93a593f1e75c45be60f1e4d5c9d31b787
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-26 15:24:42 +0500

    update

--- 05_loops/foreachloop.js
+++ 05_loops/foreachloop.js
@@ -1,14 +1,39 @@
 const coding = ["js","ruby","c++","python","java"]
 
-coding.forEach( function (items) {
-//    console.log(items);
+// coding.forEach( function (items) {
+// //    console.log(items);
    
-} )
+// } )
     
 
 // coding.forEach( (val) => {console.log(val)});
 function printme(item) {
     console.log(item);
 }
+// coding.forEach(printme)
 
+// coding.forEach((item ,index,arr) => {
+//     console.log(item ,index,arr);
+    
+// })
 
+const mycoding = [
+    {
+    languagename : "python",
+    languagefilename : "python"
+     }
+    ,{
+        languagename : "C++",
+    languagefilename : "cpp"
+    },
+    {
+        languagename : "javascript",
+    languagefilename : "js"
+    }]
+
+mycoding.forEach((item) => {
+    // console.log(item.languagefilename);
+    console.log(item.languagename);
+    
+    
+})
