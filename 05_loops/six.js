5e54eac7fa53860b07a17baf10bbbf9b6b9a251b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-10-27 20:36:34 +0500

    update

--- 05_loops/six.js
+++ 05_loops/six.js
@@ -1,5 +1,11 @@
 const coding = ["js", "ruby", "c++", "python", "java"];
 
-coding.forEach((item) => {
-  console.log(item);
+const values = coding.forEach((item) => {
+  // console.log(item);
 });
+// console.log(values);
+
+const mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newnum = mynumbers.filter((num) => num > 4);
+console.log(newnum);
