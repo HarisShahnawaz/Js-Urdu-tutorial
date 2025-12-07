9c24650ac6eeb8c412ce98f35bca6a9a4311527b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-12-07 22:32:41 +0500

    update

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,16 @@
+# javascript and classess
+
+# oop
+
+# object
+-collection of properties and methods
+-toLowerCase
+
+# why use oop
+
+# parts of oop
+
+-objects
+-prototypes
+-classes
+-intances (new,this)
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,15 @@
+const user = {
+    username : "haris",
+    logincount : 8,
+    signedin: true,
+
+     getUserDetails : function() {
+        console.log("got user details from database");
+        
+     }
+
+}
+console.log(user.username);
+console.log(user.getUserDetails());
+
+
