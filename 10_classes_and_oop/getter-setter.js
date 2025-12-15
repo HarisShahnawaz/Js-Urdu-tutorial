6733cf45809643895e4ce3bffdc89015ea771c4b
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-12-15 14:31:23 +0500

    update

--- 10_classes_and_oop/getter-setter.js
+++ 10_classes_and_oop/getter-setter.js
@@ -2,7 +2,26 @@ class User {
     constructor(email,password){
         this.email = email
         this.password = password
+ 
+    }
+  
+    get email(){
+       return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+       return this._password.toUpperCase()
+    }
+
+    set password(value){
+        this._password = value
     }
 }
-const haris =  new User("haris@example", "1234")
-console.log(haris.password);
+const haris =  new User("haris@example", "123adku")
+// console.log(haris.password);
+console.log(haris.email);
+
